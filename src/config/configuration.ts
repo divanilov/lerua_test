219594a8bb56a6c6249ddcee786e81ab949edeb2
Dim73 <ebox.di@gmail.com>
2021-11-04 12:02:09 +0300

    added configuration

---
+++ src/config/configuration.ts
@@ -0,0 +1,11 @@
+export default {
+  global: {
+    port: parseInt(process.env.PORT, 10) || 3000,
+  },
+  development: {
+    apiVersion: '1'
+  },
+  production: {
+    apiVersion: '1'
+  }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RequestMethod } from '@nestjs/common';
+import configuration from './config/configuration';
+const env = process.env.NODE_ENV || 'development';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1', {
+  app.setGlobalPrefix(`api/v${configuration[env].apiVersion}`, {
     exclude: [{ path: 'alive', method: RequestMethod.GET }],
   });
-  await app.listen(3000);
+  await app.listen(configuration.global.port);
 }
 bootstrap();
